486ea7f75442642ba8a74cd9f723a5571957f6cf
unknown <barbarasimandi@gmail.com>
2017-11-12 17:48:08 +0100

    log saved to database again, app.prop on create

--- src/main/java/com/greenfox/barbi/p2pchatapp/controller/MainController.java
+++ src/main/java/com/greenfox/barbi/p2pchatapp/controller/MainController.java
@@ -1,27 +1,13 @@
 package com.greenfox.barbi.p2pchatapp.controller;
 
-import com.greenfox.barbi.p2pchatapp.model.Log;
-import com.greenfox.barbi.p2pchatapp.repository.LogRepository;
-import com.greenfox.barbi.p2pchatapp.service.LogService;
-import javax.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
-  @Autowired
-  LogService logService;
-
-  @Autowired
-  LogRepository logRepository;
-
   @GetMapping(value = "/index")
-  public String index(HttpServletRequest request, Exception exception) {
-    logService.checkEnvironment(request, exception);
-    Log log = new Log(request);
-    logRepository.save(log);
+  public String index() {
     return "index";
   }
 }
