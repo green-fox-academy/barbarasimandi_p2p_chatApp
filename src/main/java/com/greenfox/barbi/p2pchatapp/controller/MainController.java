4f90512fd6f22be8b767dea71b6469d7292c033c
unknown <barbarasimandi@gmail.com>
2017-11-13 15:05:55 +0100

    log saved when there is a new user

--- src/main/java/com/greenfox/barbi/p2pchatapp/controller/MainController.java
+++ src/main/java/com/greenfox/barbi/p2pchatapp/controller/MainController.java
@@ -1,7 +1,11 @@
 package com.greenfox.barbi.p2pchatapp.controller;
 
 import com.greenfox.barbi.p2pchatapp.model.ChatUser;
+import com.greenfox.barbi.p2pchatapp.model.Log;
 import com.greenfox.barbi.p2pchatapp.repository.ChatUserRepository;
+import com.greenfox.barbi.p2pchatapp.repository.LogRepository;
+import com.greenfox.barbi.p2pchatapp.service.LogService;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +16,12 @@ public class MainController {
 
   @Autowired
   ChatUserRepository chatUserRepository;
+
+  @Autowired
+  LogRepository logRepository;
+
+  @Autowired
+  LogService logService;
 
   @GetMapping(value = "/")
   public String index() {
@@ -25,7 +35,10 @@ public class MainController {
   }
 
   @GetMapping(value = "/enter/user")
-  public String enter(@RequestParam String userName) {
+  public String enter(@RequestParam String userName, HttpServletRequest request) {
+    logService.checkEnvironment(request);
+    Log log = new Log(request);
+    logRepository.save(log);
     ChatUser user = new ChatUser(userName);
     chatUserRepository.save(user);
     return "enter";
